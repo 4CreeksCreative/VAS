8b77512c9c980e4c1054df8c0e39188d09a2bb5f
deepfry <jj@4cc.co>
2017-08-17 15:52:12 -0700

    localization fix

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,10 +1,9 @@
 var locale = window.location.pathname.substring(0,3)
 $(document).ready(function(){
-	
-	$('a:not(.language)').each(function(i,val){
-		console.log('changed '+$(val).attr('href') + ' to ' + locale+$(val).attr('href'))
-		$(val).attr('href',locale+$(val).attr('href'))
-
-
-})
+	if(locale === '/de' || locale === '/es'){
+		$('a:not(.language)').each(function(i,val){
+			console.log('changed '+$(val).attr('href') + ' to ' + locale+$(val).attr('href'))
+			$(val).attr('href',locale+$(val).attr('href'))
+		})
+	}
 })
