4b090dbbc0a262fde10a1a15bfa043fc94c3da50
deepfry <jj@4cc.co>
2017-08-17 17:06:19 -0700

    fixed start.js

--- start.js
+++ start.js
@@ -3,4 +3,4 @@ var Roots = require('roots');
 var vasEnglish = new Roots(__dirname, { env: 'en' })
 var vasSpanish = new Roots(__dirname, { env: 'es' })
 var vasGerman = new Roots(__dirname, { env: 'de' })
-vasSpanish.compile().then(vasGerman.compile().then(vasEnglish.watch()))
+vasSpanish.compile().then(vasGerman.compile().then(vasEnglish.compile()))
