2c37b97e1942189e21ec77eaea5b13382b7e5a7a
deepfry <jj@4cc.co>
2017-09-07 09:21:45 -0700

    removed "color--primary" and "bg--primary" classes and replaced them with actual colors

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,14 +1,13 @@
 var locale = (/\/de\/|\/es\//ig.exec(window.location.pathname)) ? /\/de\/|\/es\//ig.exec(window.location.pathname)[0].substring(0,3) : ''
 $(document).ready(function(){
 		$('a:not(.language, .social)').each(function(i,val){
-			// if($(val).is('#fbshare')){
-			// 	$(val).attr('href',"https://www.facebook.com/dialog/share?"+"app_id=407590409638822"+"&display=popup"+"&href="+window.location.href)
-			// }
 			if($(val).attr('href') != '#' && $(val).attr('href') != undefined){
 				console.log('changed '+$(val).attr('href') + ' to ' + locale+$(val).attr('href'))
 				$(val).attr('href',locale+$(val).attr('href'))
 			}
 		})
+		$('#contact-form').attr('action',locale+$('button').attr('action'))
+		
 		$('#share-icons a.facebook').attr('href', '#')
 		$('#share-icons a.twitter').attr('href', 'https://twitter.com/home?status='+window.location.href)
 		$('#share-icons a.linkedin').attr('href', 'https://www.linkedin.com/shareArticle?mini=true&url='+window.location.href+'&title='+$('#updatetitle').text()+'&summary='+$('#featuredtext').text()+'&source='+window.location.href)
