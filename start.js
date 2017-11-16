0e3099186829bb0a2dbba4e35700e300c3d122ae
deepfry <jj@4cc.co>
2017-11-16 10:14:50 -0800

    fixed error in main compile script

--- start.js
+++ start.js
@@ -3,5 +3,5 @@ var Roots = require('roots');
 var vasEnglish = new Roots(__dirname, { env: 'en' })
 var vasSpanish = new Roots(__dirname, { env: 'es' })
 var vasGerman = new Roots(__dirname, { env: 'de' })
-var vasGerman = new Roots(__dirname, { env: 'ru' })
-vasSpanish.compile().then(vasGerman.compile().then(vasEnglish.compile()))
+var vasRussian = new Roots(__dirname, { env: 'ru' })
+vasSpanish.compile().then(vasGerman.compile().then(vasEnglish.compile().then(vasRussian.compile())))
