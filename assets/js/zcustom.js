27337a60e3fb137f07fb2a8d9401356eae6753dc
deepfry <jj@4cc.co>
2018-02-21 08:47:41 -0800

    fixed script blocking URLs in contact forms

--- assets/js/zcustom.js
+++ assets/js/zcustom.js
@@ -51,7 +51,7 @@ $('form.form-email.custom-script').submit(function(e){
 	if (grecaptcha.getResponse() != ''){
 		errorText = 'Please complete captcha'
 	}
-	if (mr.forms.validateFields($('form.form-email.custom-script')) !== 1 && grecaptcha.getResponse() != '' && /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/igm.test($('textarea').text()) != true) return
+	if (mr.forms.validateFields($('form.form-email.custom-script')) !== 1 && grecaptcha.getResponse() != '' && /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/igm.test($('textarea').val()) != true) return
 	else e.preventDefault(); mr.forms.showFormError(formSuccess, formError, 1000, 5000, 500);
 
 })
