5dc579fd1d56ac444246131664b65fe630d26acd
deepfry <jj@4cc.co>
2018-06-22 08:51:00 -0700

    added error logging

--- start.js
+++ start.js
@@ -4,4 +4,10 @@ var vasEnglish = new Roots(__dirname, { env: 'en' })
 var vasSpanish = new Roots(__dirname, { env: 'es' })
 var vasGerman = new Roots(__dirname, { env: 'de' })
 var vasRussian = new Roots(__dirname, { env: 'ru' })
+
+vasSpanish.on('error',console.error)
+vasEnglish.on('error',console.error)
+vasGerman.on('error',console.error)
+vasRussian.on('error',console.error)
+
 vasSpanish.compile().then(vasGerman.compile().then(vasEnglish.compile().then(vasRussian.compile())))
