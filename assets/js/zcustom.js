ebde7edb068103223f166a7afb4e22e10ec9ee4a
deepfry <jj@4cc.co>
2018-07-25 10:55:32 -0700

    removed captcha check from email script

--- assets/js/zcustom.js
+++ assets/js/zcustom.js
@@ -56,9 +56,9 @@ $('#contact-form').submit(function(e){
 	submitButton.attr('data-text', submitButton.text());
 	errorText = thisForm.attr('data-error') ? thisForm.attr('data-error') : "Please fill all fields correctly";
 	successText = thisForm.attr('data-success') ? thisForm.attr('data-success') : "Thanks, we'll be in touch shortly";
-	if (grecaptcha.getResponse() === ''){
-		errorText = 'Please complete captcha'
-	}
+	// if (grecaptcha.getResponse() === ''){
+	// 	errorText = 'Please complete captcha'
+	// }
 	if (/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/igm.test($('textarea').val()) === true){
 		errorText = 'Please do not include URLs in the message body'
 	}
